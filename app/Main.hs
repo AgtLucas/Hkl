f95ce37b73504c3cfe925cf77a8169f28a5e455b
Lucas <lucas@agtlucas.com>
2016-01-19 10:55:34 -0200

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import Lib
+import System.IO (readFile)
 
 main :: IO ()
 main = someFunc
 
 greet name = "Hello " ++ name ++ "!"
+
+printNumbers = do
+  putStrLn (show (3+4))
+
+printConfig = do
+  contents <- readFile "stack.yaml"
+  putStrLn contents
